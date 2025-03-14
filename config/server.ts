65040533a9b557ec50c5fc111abcb04026f4a6ed
davewevans <davewevans72@gmail.com>
2025-03-14 18:56:33 -0400

    Add logging in server config

--- config/server.ts
+++ config/server.ts
@@ -1,3 +1,10 @@
+console.log("Server Config:", {
+  host: process.env.HOST,
+  port: process.env.PORT,
+  database_host: process.env.DATABASE_HOST,
+  database_name: process.env.DATABASE_NAME,
+});
+
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
